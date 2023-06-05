a8843cc71a6e1e1e388daaf33f4a03f9e8b94416
voldemar83 <119428804+voldemar83@users.noreply.github.com>
2023-06-05 13:21:25 +0300

    Add files via upload

---
+++ ComplexCalculator/src/CalculableFactory.java
@@ -0,0 +1,8 @@
+
+// "Фабрика", создающая "продукт" - калькулятор.
+public class CalculableFactory implements iCalculableFactory {
+    
+    public iCalculable create(double primaryReal, double primaryImaginary) {
+        return new Calculator(primaryReal, primaryImaginary);
+    }
+}
---
+++ ComplexCalculator/src/iCalculable.java
@@ -0,0 +1,10 @@
+
+// Для всех операций принимаем два числа - действительную часть комплексного числа и мнимую.
+// На семинаре мы принимали целые числа.
+// Здесь придётся использовать double, т.к. будет деление, а оно не всегда целочсленное.
+public interface iCalculable {
+    iCalculable sum(double real, double imaginary);
+    iCalculable multi(double real, double imaginary);
+    iCalculable devide(double real, double imaginary);
+    double[] getResult();
+}
